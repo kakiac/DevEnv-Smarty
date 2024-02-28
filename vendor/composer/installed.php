1c18cd22f901e0d0f1342957af65d40e5b604c22
Κakia Chatsiou <kakiac@gmail.com>
2024-02-28 11:33:50 +0000

    modified:   .devcontainer/devcontainer.json 	modified:   README.md 	modified:   vendor/composer/installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7ccef2042297a4dd43705be8cb2ba79b398c40d5',
+        'reference' => 'f8dae6b7d14a592f94c33954868d305dcdeb2091',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7ccef2042297a4dd43705be8cb2ba79b398c40d5',
+            'reference' => 'f8dae6b7d14a592f94c33954868d305dcdeb2091',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
